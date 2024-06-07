e8880fe9564e8a71fc319af2ae321c27da375e77
Sujjigithub <jangilisudhakar7@gmail.com>
2024-06-07 14:22:10 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ServicesComponent } from './services/services.component';
+
+
+var myRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  {path:"services",component:ServicesComponent},
+  { path: "contact", component: ContactComponent }
+];
+
+var myRoutes2 =RouterModule.forRoot(myRoutes);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    myRoutes2
   ],
   providers: [],
   bootstrap: [AppComponent]
